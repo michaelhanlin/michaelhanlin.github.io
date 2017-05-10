e7b3209f54521858d621b98d62da5ed074b3fb0e
Ming <mingchen@users.noreply.github.com>
2017-05-10 13:18:52 -0700

    Update delpoy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,6 +7,9 @@ setup_git() {
     git config --global user.name "Travis CI"
 
     git clone https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git $DEPLOY_DIR
+
+    git rev-parse --short HEAD > $DEPLOY_DIR/version.txt
+
     (cd $DEPLOY_DIR; git branch; git branch -a; git checkout master)
 }
 
@@ -17,14 +20,16 @@ commit_website_files() {
     #rm -fr *
     rm -fr $DEPLOY_DIR/*
     cp -r public/* $DEPLOY_DIR/
+
     cd $DEPLOY_DIR
+
     git add .
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-    git status
-    ls
-    find .
+
     #git remote add origin-master https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git > /dev/null 2>&1
     #git push --quiet --set-upstream origin-master master
+
+    git push
 }
 
 setup_git
