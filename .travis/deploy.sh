76d3dc883948df88a0f9e06873954dddeb4623c4
Ming <mingchen@users.noreply.github.com>
2017-05-10 12:00:42 -0700

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,12 +5,20 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+git_clone() {
+    mkdir .deploy_git
+    git clone https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git .deploy_git
+    (cd .deploy_git; git branch; git branch -a; git checkout master)
+}
+
 commit_website_files() {
-    mv public /tmp/public
-    git remote update
-    git checkout master
-    rm -fr *
-    cp -r /tmp/public/* .
+    #mv public /tmp/public
+    #git remote update
+    #git checkout master
+    #rm -fr *
+    rm -fr .deploy_git/*
+    cp -r public/* .deploy_git/
+    cd .deploy_git
     git add .
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
     git status
