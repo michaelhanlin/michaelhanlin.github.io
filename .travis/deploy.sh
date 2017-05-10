bc404ca98bc59efe4f386c9fbeeb154046e28a38
Ming <mingchen@users.noreply.github.com>
2017-05-10 13:10:02 -0700

    Update delpoy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+DEPLOY_DIR=.deploy_git
 
-git_clone() {
-    mkdir .deploy_git
-    git clone https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git .deploy_git
-    (cd .deploy_git; git branch; git branch -a; git checkout master)
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+
+    git clone https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git $DEPLOY_DIR
+    (cd $DEPLOY_DIR; git branch; git branch -a; git checkout master)
 }
 
 commit_website_files() {
@@ -16,21 +15,17 @@ commit_website_files() {
     #git remote update
     #git checkout master
     #rm -fr *
-    rm -fr .deploy_git/*
-    cp -r public/* .deploy_git/
-    cd .deploy_git
+    rm -fr $DEPLOY_DIR/*
+    cp -r public/* $DEPLOY_DIR/
+    cd $DEPLOY_DIR
     git add .
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
     git status
     ls
     find .
-}
-
-upload_files() {
-  git remote add origin-master https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git > /dev/null 2>&1
-  #git push --quiet --set-upstream origin-master master
+    #git remote add origin-master https://${REPO_TOKEN}@github.com/michaelhanlin/michaelhanlin.github.io.git > /dev/null 2>&1
+    #git push --quiet --set-upstream origin-master master
 }
 
 setup_git
 commit_website_files
-upload_files
